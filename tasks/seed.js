0573fdbe0764fd4fe8a77bcbfd54f6c1628b44f7
Simon Gao <sgao1202@gmail.com>
2020-11-20 01:04:05 -0500

    uncommented comment creation

--- tasks/seed.js
+++ tasks/seed.js
@@ -29,20 +29,20 @@ async function main() {
         console.log();
     }
     // Create comments
-    // let cAu1 = await comments.createComment("reviewA", "user1", "hello a1");
-    // let cBu1 = await comments.createComment("reviewB", "user1", "hello b1");
-    // let cAu2 = await comments.createComment("reviewA", "user2", "hello a2");
-    // let cBu2 = await comments.createComment("reviewB", "user2", "hello b2");
-    // let cAu3 = await comments.createComment("reviewA", "user3", "hello a3");
-    // let cBu3 = await comments.createComment("reviewB", "user3", "hello b3");
+    let cAu1 = await comments.createComment("reviewA", "user1", "hello a1");
+    let cBu1 = await comments.createComment("reviewB", "user1", "hello b1");
+    let cAu2 = await comments.createComment("reviewA", "user2", "hello a2");
+    let cBu2 = await comments.createComment("reviewB", "user2", "hello b2");
+    let cAu3 = await comments.createComment("reviewA", "user3", "hello a3");
+    let cBu3 = await comments.createComment("reviewB", "user3", "hello b3");
 
-    // console.log(await comments.getCommentById(cAu1._id));
-    // console.log(await comments.getAllCommentsOfReview("reviewA"));
-    // console.log(await comments.getAllCommentsOfUser("user1"));
-    // await comments.deleteComment(cAu1._id);
+    console.log(await comments.getCommentById(cAu1._id));
+    console.log(await comments.getAllCommentsOfReview("reviewA"));
+    console.log(await comments.getAllCommentsOfUser("user1"));
+    await comments.deleteComment(cAu1._id);
 
-    // console.log(await comments.getAllCommentsOfReview("reviewA"));
-    // console.log(await comments.getAllCommentsOfUser("user1"));
+    console.log(await comments.getAllCommentsOfReview("reviewA"));
+    console.log(await comments.getAllCommentsOfUser("user1"));
 
     console.log('Done seeding database');
     await db.serverConfig.close();
