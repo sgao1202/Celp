714db3f6737cffaee6c10441a53e1808b762b355
Mmonaco13 <mmonaco1@stevens.edu>
2020-11-22 23:07:22 +0000

    progress towards complete db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const seed = require("./tasks/seed");
+/*const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
 
@@ -9,4 +10,4 @@ configRoutes(app);
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log('Your routes will be running on http://localhost:3000');
-});
+});*/
--- tasks/seed.js
+++ tasks/seed.js
@@ -9,7 +9,7 @@ async function main() {
     const db = await dbConnection();
     await db.dropDatabase();
 
-    // Create restaurants
+    // Create Restaurants
     const restaurant1 = await restaurants.createRestaurant('McDonalds', '234 Washington St', 'Fast Food');
     const restaurant2 = await restaurants.createRestaurant('QDOBA Mexican Eats', '400 Washington St', 'Mexican');
     const restaurant3 = await restaurants.createRestaurant('T Thai', '102 Hudson St', 'Thai');
@@ -17,7 +17,7 @@ async function main() {
     const restaurant5 = await restaurants.createRestaurant("O'Bagel", '600 Washington St, Hoboken', 'Breakfast');
     const restaurant6 = await restaurants.createRestaurant('Chicken Factory', '529 Washington St', 'Korean');
 
-    // Create users
+    // Create Users
     const user1 = await users.createUser('Simon', 'Smells', 'ss20@gmail.com', 'stinkyman20', 25, 'stonkystonk');
     const user2 = await users.createUser('Taquisha', 'Maramba', 'tmaram02@gmail.com', 'tqmaram095', 44, 'shakalaka');
     const user3 = await users.createUser('Brett', 'King', 'bretkingcrown@gmail.com', 'bigthrone39', 18, 'royaltyawaits');
@@ -25,10 +25,31 @@ async function main() {
     const user5 = await users.createUser('Annie', 'Patel', 'hehexd@gmail.com', 'secretlover21', 20, 'smoochcooch');
     const user6 = await users.createUser('Jon', 'Con', 'jancan99@gmail.com', 'ConBoy05', 10, 'robbinGbanks');
 
+    console.log(1);
+    
+    // Create Reviews
+    const r1u1 = await reviews.createReview(user1._id, restaurant1._id, "This restaurant is great", 5,
+        {distancedTables: true, maskedEmployees: true, noTouchPayment: true, outdoorSeating: true});
+    const r1u2 = await reviews.createReview(user2._id, restaurant1._id, "This restaurant was okay", 3,
+        {distancedTables: true, maskedEmployees: true, noTouchPayment: false, outdoorSeating: false});
+    const r2u1 = await reviews.createReview(user1._id, restaurant2._id, "This restaurant is terrible", 1,
+        {distancedTables: false, maskedEmployees: false, noTouchPayment: false, outdoorSeating: true});
+    const r2u2 = await reviews.createReview(user2._id, restaurant2._id, "This restaurant was not great", 2,
+        {distancedTables: true, maskedEmployees: true, noTouchPayment: false, outdoorSeating: true});
+
+    // Create Comments
+    let cAu1 = await comments.createComment(r1u1._id, user1._id, "Hello myself");
+    let cBu1 = await comments.createComment(r1u2._id, user1._id, "I agree");
+    let cAu2 = await comments.createComment(r1u1._id, user2._id, "completely wrong");
+    let cBu2 = await comments.createComment(r1u2._id, user2._id, "This is my review");
+    let cAu3 = await comments.createComment(r1u1._id, user3._id, "how safe did you feel?");
+    let cBu3 = await comments.createComment(r1u2._id, user3._id, "great review");
+
     /* test for case sensitive username and email */
     // const user11 = await users.createUser('Simon', 'Smells', 'SS20@gMAil.COM', 'stinkyman20', 25, 'stonkystonk');
     // const user12 = await users.createUser('Simon', 'Smells', 'smmm20@gmail.com', 'stiNkyMan20', 25, 'stonkystonk');
 
+    /*
     //test getUserbyId
     try{
         console.log(await users.getUserById(user2._id.toString()));
@@ -55,14 +76,10 @@ async function main() {
         console.log('Error caught');
         console.log();
     }
-    // Create comments
-    let cAu1 = await comments.createComment("reviewA", "user1", "hello a1");
-    let cBu1 = await comments.createComment("reviewB", "user1", "hello b1");
-    let cAu2 = await comments.createComment("reviewA", "user2", "hello a2");
-    let cBu2 = await comments.createComment("reviewB", "user2", "hello b2");
-    let cAu3 = await comments.createComment("reviewA", "user3", "hello a3");
-    let cBu3 = await comments.createComment("reviewB", "user3", "hello b3");
 
+    // Test Comments
+
+    }
     console.log(await comments.getCommentById(cAu1._id));
     console.log(await comments.getAllCommentsOfReview("reviewA"));
     console.log(await comments.getAllCommentsOfUser("user1"));
@@ -70,7 +87,7 @@ async function main() {
 
     console.log(await comments.getAllCommentsOfReview("reviewA"));
     console.log(await comments.getAllCommentsOfUser("user1"));
-
+    */
     console.log('Done seeding database');
     await db.serverConfig.close();
 }
