7a6af223ee636bd7684ee119caef2b608de459b1
Simon Gao <sgao1202@gmail.com>
2020-11-22 03:38:17 -0500

    added create form for restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,7 +6,7 @@ const restaurantData = data.restaurants;
 // Route for the page of all restaurants
 router.get('/', async (req, res) => {
     const restaurants = await restaurantData.getAllRestaurants();
-    console.log(restaurants);
+    // console.log(restaurants);
     res.render('restaurants/all', {restaurants: restaurants});
 });
 
@@ -15,9 +15,16 @@ router.get('/:id', async (req, res) => {
 
 });
 
-// Route to create a restaurant
-router.post('/create', async (req, res) => {
+// Get create a restaurant page
+router.get('/new', async (req, res) => {
+    console.log('Entered GET route for restaurants/new');
+    res.render('restaurants/new');
+});
 
+// Route to create a restaurant
+router.post('/new', async (req, res) => {
+    let newRestaurantData = req.body;
+    console.log(newRestaurantData);
 });
 
 module.exports = router;
