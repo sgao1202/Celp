56b44d46e5e48c15091dd07fabbb1730c12969d4
Albert Chen <achen17@stevens.edu>
2020-11-22 20:54:18 -0500

    set handlebar settings in main

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
+const static = express.static(__dirname + '/public');
 
-// const seed = require("./tasks/seed");
+app.use('/public', static);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// const seed = require("./tasks/seed");
 configRoutes(app);
 
 app.listen(3000, () => {
