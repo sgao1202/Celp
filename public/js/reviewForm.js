47615f0c75a41b8eb77d7957b898d97e95432cf2
Simon Gao <sgao1202@gmail.com>
2020-12-06 21:01:03 -0500

    added client-side form validation for reviews

--- public/js/reviewForm.js
+++ public/js/reviewForm.js
@@ -1,8 +1,55 @@
 (function($) {
-    $('#review-form').submit((event) => {
+
+    let hasErrors = false;
+    function validString(str) {
+        if (!str || !isNaN(str)) {
+            hasErrors = true;
+            return false;
+        }
+        return true;
+    }
+
+    function validNumber(num) {
+        // Must be a whole integer between 1 and 5
+        if (!num || isNaN(num) || num.includes('.') || parseInt(num) < 1 || parseInt(num) > 5) {
+            hasErrors = true;
+            return false;
+        }
+        return true;
+    }
+
+    let form = $('#review-form');
+    let ratingInput = $('#review-rating');
+    let priceInput = $('#review-price');
+    let distancedTablesInput = $('#distanced-tables');
+    let maskedEmployeesInput = $('#masked-employees');
+    let noTouchPaymentInput = $('#no-touch-payment');
+    let outdoorDiningInput = $('#outdoor-dining');
+    let reviewTextInput = $('#review-text');
+
+    form.submit((event) => {
         // Client side validation
         event.preventDefault();
-        
+        hasErrors = false;
+        ratingInput.removeClass('is-valid is-invalid');
+        priceInput.removeClass('is-valid is-invalid');
+        reviewTextInput.removeClass('is-valid is-invalid');
+
+        let info = {
+            rating: ratingInput.val().trim(),
+            price: priceInput.val().trim(),
+            distancedTables: distancedTablesInput.prop('checked'),
+            maskedEmployeesInput: maskedEmployeesInput.prop('checked'),
+            noTouchPayment: noTouchPaymentInput.prop('checked'),
+            outdoorDining: outdoorDiningInput.prop('checked'),
+            text: reviewTextInput.val().trim()
+        };
+
+        if (!validNumber(info.rating)) ratingInput.addClass('is-invalid');
+        if (!validNumber(info.price)) priceInput.addClass('is-invalid');
+        if (!validString(info.text)) reviewTextInput.addClass('is-invalid');
+
+        if (!hasErrors) form.unbind().submit();
     });
 
 })(jQuery);
