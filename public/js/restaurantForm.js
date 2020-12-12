040993266449188a18fcc12dca097148936818b7
Mingyu Yao <yaomingyu.umd@gmail.com>
2020-12-12 14:16:04 -0500

    1. If no results are found through filtering, 	display a message that says “No results found. 2. Confirm links redirect to new tab.
    Signed-off-by: Mingyu Yao <yaomingyu.umd@gmail.com>

--- public/js/restaurantForm.js
+++ public/js/restaurantForm.js
@@ -14,6 +14,7 @@
     let nameInput = $('#restaurant-name-input');
     let addressInput = $('#restaurant-address-input');
     let cuisineInput = $('#restaurant-cuisine-input');
+    let linkInput = $('#restaurant-link-input');
 
     form.submit((event) => {
         // Prevent default form submission
@@ -22,11 +23,13 @@
         nameInput.removeClass('is-invalid is-valid');
         addressInput.removeClass('is-invalid is-valid');
         cuisineInput.removeClass('is-invalid is-valid');
+        linkInput.removeClass('is-invalid is-valid');
 
         let info = {
             name: nameInput.val().trim(),
             address: addressInput.val().trim(),
-            cuisine: cuisineInput.val().trim()
+            cuisine: cuisineInput.val().trim(),
+            link: linkInput.val().trim()
         };
         /*
             Make POST request to server if input is valid, 
@@ -38,6 +41,7 @@
         if (!validString(info.name)) nameInput.addClass('is-invalid');
         if (!validString(info.address)) addressInput.addClass('is-invalid');
         if (!validString(info.cuisine)) cuisineInput.addClass('is-invalid');
+        if (!validString(info.link)) linkInput.addClass('is-invalid');
 
         // Unbind submit event to avoid an infinite loop
         if (noErrors) form.unbind().submit();
